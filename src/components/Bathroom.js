6fa93aefadd7a2733656ed54ec0cc156baca7a64
KuntalGhosal <ayan.pal010@gmail.com>
2021-06-23 04:01:41 +0530

    kitchen,bathroom,bedroom images in array

---
+++ src/components/Bathroom.js
@@ -0,0 +1,38 @@
+import Gltwo from "../assets/pexels-arley-bateca-3941260.jpg";
+import Glthree from "../assets/pexels-cottonbro-4153152.jpg";
+import Glfive from "../assets/pexels-curtis-adams-3288104.jpg";
+import Glsix from "../assets/pexels-hakim-santoso-3634742.jpg";
+import Glseven from "../assets/pexels-jean-van-der-meulen-1454804.jpg";
+import Alone from "../assets/pexels-deeana-creates-2534572.jpg";
+import Altwo from "../assets/pexels-joey-342800.jpg";
+import Althree from "../assets/pexels-jonathan-borba-3316926.jpg";
+import Alfive from "../assets/pexels-karolina-grabowska-4239013.jpg";
+import Alsix from "../assets/pexels-ksenia-chernaya-4450337.jpg";
+import Alseven from "../assets/pexels-markus-spiske-127726.jpg";
+import Blone from "../assets/pexels-max-vakhtbovych-6032425.jpg";
+import Bltwo from "../assets/pexels-max-vakhtbovych-6121091.jpg";
+import Blthree from "../assets/pexels-max-vakhtbovych-6198654.jpg";
+import Blfive from "../assets/pexels-max-vakhtbovych-6207818.jpg";
+import Blsix from "../assets/pexels-max-vakhtbovych-6198661.jpg";
+import Blseven from "../assets/pexels-max-vakhtbovych-6283974.jpg";
+
+export const Bathroom =
+        [Gltwo,
+            Glthree, 
+            Glfive ,
+            Glsix ,
+            Glseven, 
+            Alone ,
+            Altwo ,
+            Althree, 
+            Alfive ,
+            Alsix ,
+            Alseven ,
+            Blone ,
+            Bltwo ,
+            Blthree, 
+            Blfive ,
+            Blsix ,
+            Blseven , 
+]
+// Bathroom.
---
+++ src/components/Bedroom.js
@@ -0,0 +1,40 @@
+import Glone from "../assets/pexels-burst-545012.jpg";
+import Gltwo from "../assets/pexels-cats-coming-707581.jpg";
+import Glthree from "../assets/pexels-charlotte-may-5824497.jpg";
+import Glfive from "../assets/pexels-nastyasensei-376531.jpg";
+import Glsix from "../assets/pexels-dmitry-zvolskiy-2062431.jpg";
+import Glseven from "../assets/pexels-jean-van-der-meulen-1454806.jpg";
+import Alone from "../assets/pexels-jean-van-der-meulen-1457845.jpg";
+import Altwo from "../assets/pexels-jean-van-der-meulen-2416932.jpg";
+import Althree from "../assets/pexels-julie-aagaard-1374125.jpg";
+import Alfive from "../assets/pexels-m&w-studios-90317.jpg";
+import Alsix from "../assets/pexels-m&w-studios-90319.jpg";
+import Alseven from "../assets/pexels-max-vakhtbovych-6492396.jpg";
+import Blone from "../assets/pexels-max-vakhtbovych-6758352.jpg";
+import Bltwo from "../assets/pexels-max-vakhtbovych-6758769.jpg";
+import Blthree from "../assets/pexels-max-vakhtbovych-6782468.jpg";
+import Blfive from "../assets/pexels-max-vakhtbovych-6782469.jpg";
+import Blsix from "../assets/pexels-max-vakhtbovych-6782578.jpg";
+import Blseven from "../assets/pexels-max-vakhtbovych-6890413.jpg";
+
+export const Bedroom =
+        [Glone,
+            Gltwo,
+            Glthree, 
+            Glfive ,
+            Glsix ,
+            Glseven, 
+            Alone ,
+            Altwo ,
+            Althree, 
+            Alfive ,
+            Alsix ,
+            Alseven ,
+            Blone ,
+            Bltwo ,
+            Blthree, 
+            Blfive ,
+            Blsix ,
+            Blseven , 
+]
+// Bathroom.
